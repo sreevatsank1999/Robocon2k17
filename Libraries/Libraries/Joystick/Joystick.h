e64d5b62d471a59c0e91bd0d1a3f466582cf5805
Sreevatsank <Sreevatsank@SREEVATSANK-DES>
2018-01-28 19:36:25 +0530

    Re: Both UTF-8 and ASCII Versions Updated Improved Joystick and Joystick_Analog Class Joystick Class == A simple Output Virtual Joystick Class Joystick_Analog Class == Real Analog Joystick Class

--- Libraries/Libraries/Joystick/Joystick.h
+++ Libraries/Libraries/Joystick/Joystick.h
@@ -4,32 +4,40 @@
 #include <Arduino.h>
 
 class Joystick {                      // C++ Joystick Class  // Joystick(Axis_x, Axis_y = -1, Threshold_x = 50, Threshold_y = 50, Max_x = 512, Max_y = 512);
-protected:
-	int JVal_X,JVal_Y; float A;
-
-	const int Jx, Jy;
-
-	int OffstX, OffstY;
-
-	const int AMax_X, AMax_Y; 
-
-	const int ThrshldX, ThrshldY;
-
 public:
 	float K;
 	float CosO;
 	float SinO;
 
-	Joystick(const int Axis_x, const int Axis_y = -1, const int Threshold_x = 50, const int Threshold_y = 50, const int Max_x = 512, const int Max_y = 512);
+	Joystick();
 
 	int Joystick_Get(float &Extern_K);
 	int Joystick_Get(float &Extern_K, float &Extern_CosO);
 	int Joystick_Get(float &Extrn_K, float &Extrn_CosO, float &Extrn_SinO);    // Complete Input Function, Gives Amplitude Factor(K), Angles(CosO & SinO)
-	virtual void Initialise(); 
-	
-	virtual int Update(); 
 
-	int Joystick_Debug(); 
+	virtual int Update();
+
+	virtual int Joystick_Debug();
+};
+
+class Joystick_Analog : public Joystick {
+protected:
+	int JVal_X, JVal_Y; float A;
+
+	const int Jx, Jy;
+
+	int OffstX, OffstY;
+
+	const int AMax_X, AMax_Y;
+
+	const int ThrshldX, ThrshldY;
+
+public:
+	Joystick_Analog(const int Axis_x, const int Axis_y = -1, const int Threshold_x = 50, const int Threshold_y = 50, const int Max_x = 512, const int Max_y = 512);
+
+	virtual void Initialise();
+	virtual int Update();
+	virtual int Joystick_Debug();
 
 protected:
 	bool Read();
@@ -39,12 +47,5 @@ protected:
 
 	int A_Cos_Sin();
 	int A_Cos_Sin(int X, int Y, float &Amp, float &Cosα, float &Sinα);
-};
-
-class Joystick_Analog : public Joystick {
-	
-public:	
-	Joystick_Analog(const int Axis_x, const int Axis_y = -1, const int Threshold_x = 50, const int Threshold_y = 50, const int Max_x = 512, const int Max_y = 512);
-	void RawRead(int &X, int &Y);
 };
 #endif // !Joystick_h 
