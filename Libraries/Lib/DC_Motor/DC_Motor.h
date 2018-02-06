5ea33bc919194d7818c408c3cfc06ca9768e6d76
Sreevatsank <Sreevatsank@SREEVATSANK-DES>
2018-02-06 12:09:05 +0530

    Updated: Documeted DC_Motor Class Added: Wheel Class, MotorAssmbly Class Added procedures to get Real World Velocity

--- Libraries/Lib/DC_Motor/DC_Motor.h
+++ Libraries/Lib/DC_Motor/DC_Motor.h
@@ -8,9 +8,9 @@ class DC_Motor {                      // C++ DC_Motor Class    // M(PWM_pin, DIR
 	const int M_PWM;
 	const int M_DIR;
 
-	const float Rated_RPM;				
+	const float Rated_RPM;					// Physical RPM
 	
-	const float rpm_limit;
+	const float rpm_limit;					// Physical RPM
 	const int pwm_Bias;
 
 public:
@@ -28,4 +28,23 @@ public:
 	void Drive(const float k);
 	void Drive(const float k, const float rpm);
 };
+
+class Wheel {
+public:
+	const float d;						// Physical Diameter
+
+	Wheel(const float Diameter = 0.0);
+};
+
+template<class MotorClass>
+class MotorAssmbly : public MotorClass : public Wheel {
+public:
+	const float Vmax;					// Real World Velocity Max(in Units)
+
+	MotorAssmbly(const MotorClass Motor, const Wheel wheel);
+	MotorAssmbly(const MotorClass Motor, const Wheel wheel, const float Max_V);
+
+	float Get_V();					// Real World Velocity(Realtime)
+};
+
 #endif // !DC_Motor_h
