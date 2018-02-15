339a42528dfdb90677f0ce7cdc57621dad8a1fdb
Sreevatsank <Sreevatsank@SREEVATSANK-DES>
2018-02-16 03:24:26 +0530

    Major : Changed rpm_limit to rpm_limit_k (0-100%) Added Constructors in MotorAssmbly Class

--- Libraries/Lib/DC_Motor/DC_Motor.cpp
+++ Libraries/Lib/DC_Motor/DC_Motor.cpp
@@ -1,14 +1,14 @@
 #include "DC_Motor.h"
 
-DC_Motor::DC_Motor(const int PWM_pin, const int DIR_pin, const float rated_rpm, const float Max_rpm, const int Bias_pwm)
+DC_Motor::DC_Motor(const int PWM_pin, const int DIR_pin, const float rated_rpm, const float rpm_k, const int Bias_pwm)
 	: M_PWM(PWM_pin), M_DIR(DIR_pin), Rated_RPM(rated_rpm),
-		rpm_limit(Max_rpm) , pwm_Bias(Bias_pwm) 
+		rpm_limit_k(rpm_k) , pwm_Bias(Bias_pwm) 
 {
 	Vr = 0.0;
 }
-DC_Motor::DC_Motor(const DC_Motor &Motor, const float Max_rpm) 
+DC_Motor::DC_Motor(const DC_Motor &Motor, const float rpm_k) 
 	: M_PWM(Motor.M_PWM), M_DIR(Motor.M_DIR), Rated_RPM(Motor.Rated_RPM),
-		rpm_limit(Max_rpm),	pwm_Bias(Motor.pwm_Bias) 
+		rpm_limit_k(rpm_k),	pwm_Bias(Motor.pwm_Bias) 
 {
 	Vr = 0.0;
 }
@@ -19,7 +19,7 @@ void DC_Motor::Initialise() {
 }
 
 inline float DC_Motor::Get_rpm() {
-	return Vr*rpm_limit;
+	return Vr*rpm_limit_k*Rated_RPM;
 }
 
 inline unsigned int DC_Motor::rpm_to_pwm(const float rpm) {	
@@ -29,19 +29,19 @@ inline unsigned int DC_Motor::rpm_to_pwm(const float rpm) {
 }
 
 void DC_Motor::Drive() {
-	Drive(Vr, rpm_limit);
+	Drive(Vr, rpm_limit_k);
 }
 void DC_Motor::Drive(const float k) {
 	Vr = k;
-	Drive(Vr, rpm_limit);
+	Drive(Vr, rpm_limit_k);
 }
-void DC_Motor::Drive(const float k, const float Max_rpm) {
+void DC_Motor::Drive(const float k, const float rpm_k) {
 	// High => CounterClockwise  , Low => Clockwise
 
 	if (k > 0)   digitalWrite(M_DIR, HIGH);
 	else          digitalWrite(M_DIR, LOW);
 
-	const int pwm = abs(k) * rpm_to_pwm(rpm_limit);
+	const int pwm = abs(k) * rpm_to_pwm(rpm_k*Rated_RPM);
 
 	analogWrite(M_PWM, pwm);
 }
--- Libraries/Lib/DC_Motor/DC_Motor.h
+++ Libraries/Lib/DC_Motor/DC_Motor.h
@@ -3,21 +3,21 @@
 
 #include <Arduino.h>
 
-class DC_Motor {                      // C++ DC_Motor Class    // M(PWM_pin, DIR_pin, Rated_RPM, rpm_limit=220, pwm_Bias=0);
+class DC_Motor {                      // C++ DC_Motor Class    // M(PWM_pin, DIR_pin, Rated_RPM, rpm_limit_k = 1.0, pwm_Bias=0);
 
 	const int M_PWM;
 	const int M_DIR;
 protected:
 	const float Rated_RPM;					// Physical RPM
 	
-	const float rpm_limit;					// Physical RPM
+	const float rpm_limit_k;					// Physical RPM
 	const int pwm_Bias;
 
 public:
 	float Vr;            // Vr => V/Vm, Vm - Max Motor Velocity
 
-	DC_Motor(const int PWM_pin, const int DIR_pin, const float rated_rpm, const float Max_rpm = 220, const int Bias_pwm = 0);
-	DC_Motor(const DC_Motor &Motor, const float Max_rpm);
+	DC_Motor(const int PWM_pin, const int DIR_pin, const float rated_rpm, const float rpm_k = 1.0, const int Bias_pwm = 0);
+	DC_Motor(const DC_Motor &Motor, const float rpm_k);
 
 	void Initialise();
 
@@ -45,11 +45,15 @@ public:
 
 	MotorAssmbly(const MotorClass Motor, const Wheel wheel)
 		:MotorClass(Motor), Wheel(wheel),
-		 Vmax((MotorClass::rpm_limit / 60)*PI*d) {}
+		 Vmax((MotorClass::rpm_limit_k*(MotorClass::Rated_RPM) / 60)*PI*d) {}
 
 	MotorAssmbly(const MotorClass Motor, const Wheel wheel, const float Max_V)
 		:MotorClass(Motor), Wheel(wheel),
 		 Vmax(Max_V) {}
+
+	MotorAssmbly(MotorAssmbly &MotorAsm, const float Max_V)
+		:MotorClass(MotorAsm), Wheel(MotorAsm),
+		Vmax(Max_V) {}
 
 	inline float Get_V() {					// Real World Velocity(Realtime)
 		return MotorClass::Vr*Vmax;
